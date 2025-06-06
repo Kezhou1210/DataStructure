c6eeff9c3deaf713c2515fdf1ca569004f85cb9e
kezhou <kezhou1210@gmail.com>
2025-06-06 11:01:05 +0800

    modified Collatez.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,3 +1,4 @@
+package DataStructure.lab1;
 /** Class that prints the Collatz sequence starting from a given number.
  *  @author YOUR NAME HERE
  */
@@ -10,7 +11,7 @@ public class Collatz {
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
